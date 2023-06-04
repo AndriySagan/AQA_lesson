dbffa6ce0894037a81849a37d56f97c5bb5b20fd
AndriySagan <sagan.a.v.g@gmail.com>
2023-06-04 10:40:08 +0300

    Закоментував налаштув.фото відео cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -6,11 +6,11 @@ module.exports = defineConfig({
       // implement node event listeners here
     },
   },
-  viewportWidth: 1920,
-  viewportHeight: 1080,
-  defaultCommandTimeout: 5000,
-  videosFolder: "cypress/videos",
-  video: true, //вмикаю запис відео
-  videoCompression: 16, // компресія(стискання об'єму відео за рахунок зменшення якості) за замовчуванням стоїть 32
-  videoUploadOnPasses: false, // false, відео буде завантажуватися тільки в разі збою тесту або його неуспішного проходження.
+  // viewportWidth: 1920,
+  // viewportHeight: 1080,
+  // defaultCommandTimeout: 5000,
+  // videosFolder: "cypress/videos",
+  // video: true, //вмикаю запис відео
+  // videoCompression: 16, // компресія(стискання об'єму відео за рахунок зменшення якості) за замовчуванням стоїть 32
+  // videoUploadOnPasses: false, // false, відео буде завантажуватися тільки в разі збою тесту або його неуспішного проходження.
 });
